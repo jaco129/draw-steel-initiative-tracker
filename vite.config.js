e51d026f7d3ec066894c510a08cc6aa555f80c8a
Seiler, Jacob <jseiler@fmi.com>
2025-04-01 10:34:37 -0500

    Trash

--- vite.config.js
+++ vite.config.js
@@ -2,6 +2,9 @@ import { sveltekit } from '@sveltejs/kit/vite';
 
 export default {
   plugins: [sveltekit()],
+  resolve: {
+    extensions: ['.mjs', '.js', '.ts', '.json', '.svelte']
+  },
   css: {
     postcss: null // Remove or set to null if explicitly defined
   }
